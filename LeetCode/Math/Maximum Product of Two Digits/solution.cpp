2aa94010725ca21691de1781b383af8031345e0b
Manjot Singh <pritpallakhu78@gmail.com>
2026-07-30 22:58:11 +0530

    Solved #3536 Maximum Product of Two Digits

--- LeetCode/Math/Maximum Product of Two Digits/solution.cpp
+++ LeetCode/Math/Maximum Product of Two Digits/solution.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int>arr;
-        int maxcount=0;
-        int count=0;
-        int m=n;
-        while(m>0)
+
+        int first = 0;
+        int second = 0;
+
+        while(n > 0)
         {
-            int temp=m%10;
-            arr.push_back(temp);
-            count++;
-            m=m/10;
+            int digit = n % 10;
+
+            if(digit > first)
+            {
+                second = first;
+                first = digit;
+            }
+            else if(digit > second)
+            {
+                second = digit;
+            }
+
+            n /= 10;
         }
 
-        sort(arr.begin(),arr.end());
-
-        return arr[count-1]*arr[count-2];
+        return first * second;
     }
 };
